0dc094b054cb718a8eead77178151bc2b982136e
Elijah Mbaabu <elijahmbaabu1@gmail.com>
2021-11-18 16:45:41 +0300

    Fronted Interaction done

--- app/src/main/java/com/example/debtcontrol/Home.java
+++ app/src/main/java/com/example/debtcontrol/Home.java
@@ -2,13 +2,37 @@ package com.example.debtcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+
+    ImageView edit, add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        edit = findViewById(R.id.imageView4);
+        add = findViewById(R.id.imageView3);
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, Edit.class));
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Home.this, AddDebtor.class));
+            }
+        });
+
     }
 }
